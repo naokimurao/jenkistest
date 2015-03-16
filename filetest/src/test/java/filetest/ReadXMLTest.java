86933b4b0fb0c3f2331365fe78fed1bc60e942ea
Naoki <nmurao@starfishassociates.com>
2015-03-16 14:24:30 -0400

    jmockit

--- filetest/src/test/java/filetest/ReadXMLTest.java
+++ filetest/src/test/java/filetest/ReadXMLTest.java
@@ -3,14 +3,22 @@ package filetest;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+
+import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 import org.naoki.ReadXML;
 
+
+
 public class ReadXMLTest {
 	
 	private ReadXML readxml;
-	
+	private @Mocked List<HierarchicalConfiguration> hierarchicalConfigurations;
 
 	@Before
 	public void setUp() throws Exception {
@@ -20,6 +28,15 @@ public class ReadXMLTest {
 	@Test
 	public void shouldReturnFalseIfInputIsSmallerThanZero() {
 		assertThat(readxml.validateInput(-7), is(false));
+	}
+	
+	@Test
+	public void test2() {
+		new NonStrictExpectations() {{
+			hierarchicalConfigurations.isEmpty(); result = false;
+	    }};
+	    
+	    assertThat(hierarchicalConfigurations.isEmpty(), is(false));
 	}
 	
 	
